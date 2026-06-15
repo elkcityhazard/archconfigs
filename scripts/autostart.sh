ec14f34357f31e2144e36ea291c8f87952c83620
Andrew M McCall <andrew.m.mccall@gmail.com>
2026-06-14 21:21:59 -0400

    added polkit service to autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
+/usr/lib/polkit-kde-authentication-agent-1 &
 wl-paste --watch cliphist store &
-
 waybar > /dev/null 2>&1 &
-
 ~/.config/scripts/wallpaper.sh > /dev/null 2>&1 &
 
 swayidle -w \
