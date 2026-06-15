75eaa64f9583b1137479274f3557fb936edfaf1f
Andrew M McCall <andrew.m.mccall@gmail.com>
2026-06-14 20:39:32 -0400

    added wallpaper script
    duplicate swayidle command in autostart.sh removed

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,10 +2,12 @@
 
 awww-daemon > /dev/null 2>&1 &
 /usr/lib/polkit-kde-authentication-agent-1 > /dev/null 2>&1 &
-
+wl-paste --watch cliphist store &
+waybar > /dev/null 2>&1 &
 swayidle -w \
-  timeout 180 'swaylock -f' \
-  timeout 300 'swaymsg "output * power off"' \
+  timeout 180 'swaylock -f -c 000000' \
+  timeout 240 'swaymsg "output * power off"' \
+  timeout 300 'systemctl hibernate' \
   resume 'swaymsg "output * power on"' \
-  before-sleep 'swaylock -f' > /dev/null 2>&1 &
+  before-sleep 'swaylock -f -c 000000'
 
